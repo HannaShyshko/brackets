3b97fe69bcd66a30b1575fa8e3e45085493c8547
Hanna Shyshko <Hanna_Shyshko@epam.com>
2018-03-01 00:07:02 +0300

    brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
-}
+
+   var stack = [];
+   
+    for (i = 0; i < str.length; i++) {
+
+        const match = bracketsConfig.find(b => b.includes(str[i]));
+
+        if(str[i] == match[1] && match[0] == stack[stack.length - 1]) {
+          stack.pop()
+          } else { 
+          stack.push(str[i]);
+        }
+    }
+    return !stack.length; 
+} 
