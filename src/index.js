6bc4a8d447b2347b7e416da691319f2e55f5817d
Hanna Shyshko <Hanna_Shyshko@epam.com>
2018-03-01 06:37:20 +0300

    brackets

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ module.exports = function check(str, bracketsConfig) {
    
     for (i = 0; i < str.length; i++) {
 
-        const match = bracketsConfig.find(b => b.includes(str[i]));
+        const match = bracketsConfig.find(bracketsConfig => bracketsConfig.includes(str[i]));
+     
 
         if(str[i] == match[1] && match[0] == stack[stack.length - 1]) {
           stack.pop()
